b532fdf18c754916aeb60ca3ff40ac4d7f4e2569
shz-shz <1210428651@qq.com>
2024-07-28 11:08:47 +0800

    Add Code First Approach

---
+++ FIT5032_MyModelFirst/Models2/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using FIT5032_MyModelFirst.Models2;
+
+namespace FIT5032_MyModelFirst.Models2
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Unit> Units { get; set; }
+    }
+}
---
+++ FIT5032_MyModelFirst/Models2/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIT5032_MyModelFirst.Models2
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
